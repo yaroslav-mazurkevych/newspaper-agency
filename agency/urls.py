ea71428a1d543636c3210450f0932c699b5119a9
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-09-29 13:45:48 +0300

    modify newspaper_agency/urls.py & add new urls.py file to agency

---
+++ agency/urls.py
@@ -0,0 +1,52 @@
+from django.urls import path
+
+from .views import (
+    index,
+    TopicListView,
+    TopicCreateView,
+    TopicUpdateView,
+    TopicUpdateView,
+    TopicDeleteView,
+    RedactorListView,
+    RedactorDetailView,
+    RedactorCreateView,
+    RedactorUpdateView,
+    RedactorDeleteView,
+    NewspaperListView,
+    NewspaperDetailView,
+    NewspaperCreateView,
+    NewspaperDeleteView,
+    NewspapersUpdateView,
+)
+
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("topics/", TopicListView.as_view(), name="topic-list", ),
+    path("topics/create/", TopicCreateView.as_view(), name="topic-create", ),
+    path("topics/<int:pk>/update/", TopicUpdateView.as_view(), name="topic-update",
+    ),
+    path("topics/<int:pk>/delete/", TopicDeleteView.as_view(), name="topic-delete", ),
+    path("redactors/", RedactorListView.as_view(), name="redactor-list"),
+    path("redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-info"),
+    path("redactors/create/", RedactorCreateView.as_view(), name="redactor-create"),
+    path("redactors/<int:pk>/update/", RedactorUpdateView.as_view(), name="redactor-update"),
+    path("redactors/<int:pk>/delete/", RedactorDeleteView.as_view(), name="redactor-delete"),
+    path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
+    path(
+        "newspapers/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-content"
+    ),
+    path("newspapers/create/", NewspaperCreateView.as_view(), name="newspaper-create"),
+    path(
+        "newspapers/<int:pk>/delete/",
+        NewspaperDeleteView.as_view(),
+        name="newspaper-delete"
+    ),
+    path(
+        "newspapers/<int:pk>/update/",
+        NewspapersUpdateView.as_view(),
+        name="newspaper-update"
+    ),
+]
+
+app_name = "agency"
