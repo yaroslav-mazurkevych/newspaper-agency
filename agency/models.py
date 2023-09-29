0e9ad2d685db4e56727be3f0abf3f12c1c0a685b
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-09-29 13:06:52 +0300

    Add models to models.py

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.username}: ({self.first_name} {self.last_name})"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateField()
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL)
+    publishers = models.ManyToManyField(Redactor, related_name="newspapers")
+
+    def __str__(self):
+        return f"{self.title} Publishers: {self.publishers}"
+
+
