94585336b351e8920c8f6964f34b2c318c7bb103
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-10-11 18:23:23 +0300

    Add blank lines to the end of files

--- agency/views.py
+++ agency/views.py
@@ -27,8 +27,6 @@ def index(request):
 
 class NewspaperListView(LoginRequiredMixin, generic.ListView):
     model = Newspaper
-    context_object_name = "newspaper_list"
-    template_name = "agency/newspaper_list.html"
     paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
