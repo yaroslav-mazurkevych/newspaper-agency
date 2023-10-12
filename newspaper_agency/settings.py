5d633ed7ba2d6f658ca290be52dc38514923ea0d
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-10-12 15:43:37 +0300

    Fix bugs

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -19,7 +19,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-print(os.environ["DATABASE_URL"])
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-@yp+z&nnwg6wv4e$*i^x8wl$=$s62k5k!_#mn7o3%u3a+0^#%p")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
